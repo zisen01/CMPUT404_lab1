955d686479b5cce25f95d4e813e85b1041ea940e
zisen01 <zisen@ualberta.ca>
2021-01-11 19:23:44 -0700

    lab1.py now downloads it self

--- lab1.py
+++ lab1.py
@@ -1,5 +1,7 @@
 import requests
-print(requests.__version__)
-r = requests.get('https://www.google.com/')
-print(r.status_code)
+#print(requests.__version__)
+#r = requests.get('https://www.google.com/')
+#print(r.status_code)
 
+r = requests.get('https://raw.githubusercontent.com/zisen01/CMPUT404_lab1/master/lab1.py')
+print(r.text)
